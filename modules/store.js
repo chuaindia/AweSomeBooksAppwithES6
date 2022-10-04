c08808cbc35bced17be9296f982b5e3bd43c8d83
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-10-04 18:59:48 +0530

    Add new modules folder at 6:59 p.m.

--- modules/store.js
+++ modules/store.js
@@ -1,33 +1,27 @@
-// Store Class: Handles Storage
 class Store {
-    static getBooks() {
-      let books;
-      if (localStorage.getItem('books') === null) {
-        books = [];
-      } else {
-        books = JSON.parse(localStorage.getItem('books'));
-      }
-  
-      return books;
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
     }
-  
-    static addBook(book) {
-      const books = Store.getBooks();
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
+    else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
-  
-    static removeBook(title) {
-      const books = Store.getBooks();
-  
-      books.forEach((book, index) => {
-        if (book.title === title) {
-          books.splice(index, 1);
-        }
-      });
-  
-      localStorage.setItem('books', JSON.stringify(books));
-    }
+    return books;
   }
-  
-  export default Store;
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(title) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+export default Store;
